425653dd3f2c7b145b508f7f4421c59371f20a2b
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2023-02-28 09:32:14 -0800

    delete trees

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - del the binary tree
+ * @tree: pointer to a tree
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if(!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
